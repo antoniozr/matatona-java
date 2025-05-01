aecc0b1646002ca1a5764e02513b4c2eb3b0f50c
Antonio Queiroz <antonioqueiroz.dev@gmail.com>
2025-04-30 22:38:14 -0300

    feat: Generics pt 01 - 06

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClasseGenericaTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Zgenerics.services.CarroRentavelService;
+
+public class ClasseGenericaTest01 {
+    public static void main(String[] args) {
+        CarroRentavelService carroRentavelService = new CarroRentavelService();
+        Carro carro = carroRentavelService.buscarCarroDisponivel();
+        System.out.println("Usando o Carro por um mês...");
+        carroRentavelService.retornarCarroAlugado(carro);
+    }
+}
